a1d7ca37756e7b53ddddcc7754019ce72ae6ae1b
Alex591 <adamalex200090@yahoo.com>
2020-04-08 14:05:32 +0300

    Create 15fel.py

---
+++ 15fel.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+tuplelista=[]
+
+while True:
+    nev=input('Adj egy nevet: ')
+    if nev=='0':
+        break
+    eletkor=int(input('Adj egy kort: '))
+    magassag=int(input('Adj egy magassagot: '))
+    tuplelista.append((nev,eletkor,magassag))
