d1b715304dde4fb8b3d41c0120d899cd52652424
Alex591 <adamalex200090@yahoo.com>
2020-04-10 17:07:17 +0300

    Update 15fel.py

--- 15fel.py
+++ 15fel.py
@@ -1,14 +1,6 @@
 
-
-
-
-
-
-
-
-
 tuplelista=[]
-
+# tuplelista=[('Anna',21,169),('Anna',21,168),('Anna',20,169),('Joska',22,180),('AAnna',20,175)]
 #beolvasás menete
 while True:
     nev=input('Adj egy nevet: ')
@@ -18,3 +10,10 @@ while True:
     magassag=int(input('Adj egy magassagot: '))
     tuplelista.append((nev,eletkor,magassag))
 #sortolás menete
+# sorted függvény:https://docs.python.org/3/howto/sorting.html
+b = sorted(sorted(tuplelista, key = lambda x : x[2]), key = lambda x : x[1], reverse = True)
+c=sorted(b,key=lambda x:x[0])
+for x in c:
+    print (x)
+
+
