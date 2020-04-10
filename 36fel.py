f1405c0cb528acf20703774ea85c94f3a7f79430
Alex591 <adamalex200090@yahoo.com>
2020-04-10 16:24:31 +0300

    Update 36fel.py

--- 36fel.py
+++ 36fel.py
@@ -1,9 +1,10 @@
 # teszt={'A':('43',0)}
 # for i in range(len(teszt['A'][0])):
 # print ('A'*int(teszt['A'][0][1]))
-# bemenet=input("Add meg a stringet:")
-# bemenet=bemenet.upper()
-bemenet='KAAAABBBCCDAAAK '
+bemenet=input("Add meg a stringet:")
+bemenet=bemenet.upper()+' '
+# bemenet='AAAABBBCCDAAA '
+# A bemenethez hottá lett adva egy space,hogy az utolsó betűk is regisztrálásra kerüljenek
 szotar={}
 count=1
 elozo=bemenet[0]
@@ -11,17 +12,25 @@ elozo=bemenet[0]
 butitott=bemenet[0]
 # butított string:Itt megmarad a betűk sorrendje,de minden betű csak egyszer szerepel
 
+
+# Szótár felépitése:
 for i in range(1,len(bemenet)):
     if elozo==bemenet[i]:
         count+=1
         elozo=bemenet[i]
     else:
-        butitott+=bemenet[i]
+        # megbizonyosodok róla,hogy az általam hozzáadott space ne kerüljön be a butitott stringbe
+        if bemenet[i]!=' ':
+            butitott+=bemenet[i]
         elozo=bemenet[i]
         if bemenet[i-1] not in szotar:
             szotar[bemenet[i-1]]=[str(count),0]
         else:
             szotar[bemenet[i-1]][0]+=str(count)
         count=1
-print(butitott)
 
+#Kiirás a butított string segitségével
+for x in butitott:
+     hanyszor=szotar[x][1]
+     print(f"{szotar[x][0][hanyszor]}{x}",end="")
+     szotar[x][1]+=1
